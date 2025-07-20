62de63a41d1abd2b956f11fd70d5842f0e4c5b49
HenryKun55 <henriqueflavio55@gmail.com>
2025-07-20 20:10:33 -0300

    remove throttle for video test

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,23 +17,23 @@ import { ThrottlerModule } from '@nestjs/throttler'
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    ThrottlerModule.forRoot([
-      {
-        name: 'short',
-        ttl: 1000,
-        limit: 3,
-      },
-      {
-        name: 'medium',
-        ttl: 10000,
-        limit: 20,
-      },
-      {
-        name: 'long',
-        ttl: 60000,
-        limit: 100,
-      },
-    ]),
+    // ThrottlerModule.forRoot([
+    //   {
+    //     name: 'short',
+    //     ttl: 1000,
+    //     limit: 3,
+    //   },
+    //   {
+    //     name: 'medium',
+    //     ttl: 10000,
+    //     limit: 20,
+    //   },
+    //   {
+    //     name: 'long',
+    //     ttl: 60000,
+    //     limit: 100,
+    //   },
+    // ]),
     DatabaseModule,
     UsersModule,
     DocumentsModule,
